c589c41e9c97bf8125bfaba89ff31ac2f8bab59f
da3mon <damon@squareup.com>
2010-05-20 22:56:02 -0700

    store lists of hashes

--- lib/extensions/abstract_adapter.rb
+++ lib/extensions/abstract_adapter.rb
@@ -1,11 +1,15 @@
 class ActiveRecord::Base
   class << self
-    def redis_benchmark(label, &blk)
-      REDIS.set(benchmark_key_for(label), Benchmark.measure(label) { blk.call }.to_json)
+    def redis_benchmark(key, &blk)
+      REDIS.lpush(benchmark_key_for(key), Benchmark.measure(Time.now.to_i * 1000) { blk.call }.to_json)
     end
 
-    def benchmark_key_for(label)
-      "benchmark:#{table_name}:#{label}"
+    def benchmark_key_for(key)
+      "benchmark:#{table_name}:#{key}"
+    end
+
+    def benchmarks_for(key)
+      REDIS.lrange(benchmark_key_for(key), 0, -1).map {|h| JSON.parse(h)}
     end
 
     %w(find first last all create update delete destroy).each do |method_name|
@@ -17,13 +21,13 @@ class ActiveRecord::Base
       alias_method_chain method_name, :redis_benchmark
 
       define_method "#{method_name}_benchmarks" do
-        JSON.parse(REDIS.get(benchmark_key_for(method_name)))
+        benchmarks_for(method_name)
       end
     end
 
     %w(save update_attributes destroy).each do |method_name|
       define_method "#{method_name}_benchmarks" do
-        JSON.parse(REDIS.get(benchmark_key_for(method_name)))
+        benchmarks_for(method_name)
       end
     end    
   end
--- spec/models/rapper_spec.rb
+++ spec/models/rapper_spec.rb
@@ -21,49 +21,57 @@ describe Rapper do
     context "class methods" do
       it "stores creation benchmarks" do
         benchmarks = Rapper.create_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores update benchmarks" do
         Rapper.update(@rapper.id, {:name => "Ludacris"})
         benchmarks = Rapper.update_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores find benchmarks" do
         Rapper.find(@rapper.id)
         benchmarks = Rapper.find_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores first benchmarks" do
         Rapper.first
         benchmarks = Rapper.first_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores last benchmarks" do
         Rapper.last
         benchmarks = Rapper.last_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores all benchmarks" do
         Rapper.all
         benchmarks = Rapper.all_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores delete benchmarks" do
         Rapper.delete(@rapper.id)
         benchmarks = Rapper.delete_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores destroy benchmarks" do
         Rapper.destroy(@rapper.id)
         benchmarks = Rapper.destroy_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
     end
 
@@ -72,25 +80,29 @@ describe Rapper do
         @rapper.name = "snoop"
         @rapper.save
         benchmarks = Rapper.save_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores update_attributes benchmarks" do
         @rapper.update_attributes(:name => "snoop")
         benchmarks = Rapper.update_attributes_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores delete benchmarks" do
         @rapper.delete
         benchmarks = Rapper.delete_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
 
       it "stores destroy benchmarks" do
         @rapper.destroy
         benchmarks = Rapper.destroy_benchmarks
-        benchmarks.should be_instance_of(Hash)
+        benchmarks.should be_instance_of(Array)
+        benchmarks.first.should be_instance_of(Hash)
       end
     end
   end
